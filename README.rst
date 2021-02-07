87491417064841d2f2b1a76494a981b36658c15a
Juho Kim <juho-kim@outlook.com>
2021-02-07 00:54:44 -0500

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,11 @@
+Nash Resolve
+============
+
+Nash Resolve is a a Python library for resolving nash equilibrium.
+
+The full documentation is available at `read the docs <https://nashresolve.readthedocs.io/>`_.
+
+
+License
+-------
+`MIT <https://choosealicense.com/licenses/mit/>`_
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+with open('README.rst', 'r') as long_description_file:
+    long_description = long_description_file.read()
+
+setup(
+    name='nashresolve',
+    version='0.0.1.dev1',
+    author='Juho Kim',
+    author_email='juho-kim@outlook.com',
+    description='A Python library for resolving nash equilibrium',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/AussieSeaweed/nashresolve',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.9',
+    install_requires=['gameframe'],
+)
